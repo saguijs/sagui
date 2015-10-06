fe4350efb4dd20b053a4c93fd297ec81901ea4c5
Paulo Ragonha <paulo@ragonha.me>
2015-10-06 22:17:28 +0200

    Loads the es5-shim from either NPM2 or NPM3 node_modules structure

--- src/karma-config-builder.js
+++ src/karma-config-builder.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import fs from 'fs'
 
 
 export function buildConfig ({ projectPath, saguiPath, webpackConfig, watch }) {
@@ -9,10 +10,7 @@ export function buildConfig ({ projectPath, saguiPath, webpackConfig, watch }) {
     browsers: ['PhantomJS'],
 
     files: [
-      // shim to workaroud PhantomJS 1.x lack of `bind` support
-      // see: https://github.com/ariya/phantomjs/issues/10522
-      path.join(saguiPath, 'node_modules/es5-shim/es5-shim.js'),
-
+      getShimPath({ projectPath, saguiPath }),
       'src/**/*.spec.*',
       { pattern: 'src/**/*', watched: true, included: false }
     ],
@@ -31,5 +29,26 @@ export function buildConfig ({ projectPath, saguiPath, webpackConfig, watch }) {
     singleRun: !watch,
 
     autoWatch: watch
+  }
+}
+
+
+/**
+  Shim to workaroud PhantomJS 1.x lack of `bind` support
+  see: https://github.com/ariya/phantomjs/issues/10522
+
+  Helper function to load the shim if the module is installed in NPM3 or NPM2
+  NPM 3 has a flatter node_modules folder.
+ */
+function getShimPath ({ projectPath, saguiPath }) {
+  const npm2Path = path.join(saguiPath, 'node_modules/es5-shim/es5-shim.js')
+  const npm3Path = path.join(projectPath, 'node_modules/es5-shim/es5-shim.js')
+
+  try {
+    fs.lstatSync(npm3Path)
+    return npm3Path
+  } catch (error) {
+    if (error.code === 'ENOENT') { return npm2Path }
+    throw error
   }
 }
