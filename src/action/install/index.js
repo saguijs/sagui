68dce44e5170381c27854ad82aa24c9c0b0e1476
Paulo Ragonha <paulo@ragonha.me>
2015-10-10 23:48:44 +0200

    Turns install action into a folder (will be expanded)

--- src/action/install/index.js
+++ src/action/install/index.js
@@ -1,5 +1,5 @@
 import { join } from 'path'
-import json from '../util/json'
+import json from '../../util/json'
 
 
 export default function install (projectPath) {
---
+++ src/action/install/index.spec.js
@@ -0,0 +1,14 @@
+describe('action/install', function () {
+  describe('package.json', function () {
+
+  });
+
+  it('should create the src folder if it doesn\'t exitst', function () {
+
+  });
+
+  it('should ', function () {
+
+  });
+
+});
