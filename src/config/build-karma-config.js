16fa64e3171c9e9b25b5e2a170c959157a5e9cc8
Paulo Ragonha <paulo@ragonha.me>
2015-10-15 22:17:17 +0200

    Delegates to 'phantomjs-shim' Karma plugin to fix the ES5 support in PhantomJS

--- src/config/build-karma-config.js
+++ src/config/build-karma-config.js
@@ -1,16 +1,11 @@
-import path from 'path'
-import fs from 'fs'
-
-
 export default function buildKarmaConfig ({ projectPath, saguiPath }, { watch }, webpackConfig) {
   return {
     basePath: projectPath,
 
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'phantomjs-shim'],
     browsers: ['PhantomJS'],
 
     files: [
-      getShimPath({ projectPath, saguiPath }),
       'src/**/*.spec.*',
       { pattern: 'src/**/*', watched: true, included: false }
     ],
@@ -29,26 +24,5 @@ export default function buildKarmaConfig ({ projectPath, saguiPath }, { watch },
     singleRun: !watch,
 
     autoWatch: watch
-  }
-}
-
-
-/**
-  Shim to workaroud PhantomJS 1.x lack of `bind` support
-  see: https://github.com/ariya/phantomjs/issues/10522
-
-  Helper function to load the shim if the module is installed in NPM3 or NPM2
-  NPM 3 has a flatter node_modules folder.
- */
-function getShimPath ({ projectPath, saguiPath }) {
-  const npm2Path = path.join(saguiPath, 'node_modules/es5-shim/es5-shim.js')
-  const npm3Path = path.join(projectPath, 'node_modules/es5-shim/es5-shim.js')
-
-  try {
-    fs.lstatSync(npm3Path)
-    return npm3Path
-  } catch (error) {
-    if (error.code === 'ENOENT') { return npm2Path }
-    throw error
   }
 }
