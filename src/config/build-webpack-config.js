48050c7d82d231eeb91ff6fc0c8316b8d10d2b08
Paulo Ragonha <paulo@ragonha.me>
2015-10-15 21:15:03 +0200

    Image support through the url-loader (inlining if smaller than 8kb)

--- src/config/build-webpack-config.js
+++ src/config/build-webpack-config.js
@@ -52,6 +52,10 @@ export default function buildWebpackConfig ({ projectPath, saguiPath }, { watch 
 
       loaders: [
         {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'url-loader?limit=8192&name=[name]-[hash].[ext]'
+        },
+        {
           test: /\.scss$/,
           // Query parameters are passed to node-sass
           loader: 'style!css!sass?outputStyle=expanded&' +
