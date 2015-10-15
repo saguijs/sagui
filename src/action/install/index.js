da2f46ce1f4b7a9bcd4efea3b1e66c54f244cb61
Paulo Ragonha <paulo@ragonha.me>
2015-10-15 20:33:00 +0200

    Install command creates a scaffold application

--- src/action/install/index.js
+++ src/action/install/index.js
@@ -1,15 +1,9 @@
-import { join } from 'path'
-import mkdirp from 'mkdirp'
-import { readFileSync, writeFileSync } from 'fs'
+import { join, basename } from 'path'
 import json from '../../util/json'
+import template from 'template-directory'
 
 
-export default function install (projectPath) {
-  updatePackageJSON(projectPath)
-  createSrcFolder(projectPath)
-  createIndexHtml(projectPath)
-  createEslintrc(projectPath)
-}
+const templatePath = join(__dirname, 'template')
 
 
 const defaultScripts = {
@@ -21,35 +15,24 @@ const defaultScripts = {
 }
 
 
+export default function install (projectPath) {
+  updatePackageJSON(projectPath)
+  copyTemplates(projectPath)
+}
+
+
+function copyTemplates (projectPath) {
+  const projectName = basename(projectPath)
+
+  template(templatePath, projectPath, undefined, {
+    projectName: projectName
+  })
+}
+
+
 function updatePackageJSON (projectPath) {
   const packagePath = join(projectPath, 'package.json')
   const content = json.read(packagePath)
   Object.assign(content.scripts, defaultScripts)
   json.write(packagePath, content)
-}
-
-
-function createSrcFolder (projectPath) {
-  const srcPath = join(projectPath, '/src')
-  mkdirp.sync(srcPath)
-}
-
-
-function createIndexHtml (projectPath) {
-  const source = join(__dirname, '/templates/index.html')
-  const target = join(projectPath, '/src/index.html')
-  cp(source, target)
-}
-
-
-function createEslintrc (projectPath) {
-  const source = join(__dirname, '/templates/eslintrc')
-  const target = join(projectPath, '/.eslintrc')
-  cp(source, target)
-}
-
-
-function cp (source, target) {
-  var content = readFileSync(source)
-  writeFileSync(target, content.toString())
 }
---
+++ src/action/install/template/src/index.spec.js
@@ -0,0 +1,5 @@
+describe('<%= projectName %>', function () {
+  xit('should work', function () {
+
+  })
+})
