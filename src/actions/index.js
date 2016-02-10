b017e6506013da3f87b4442dd00145f992150256
Paulo Ragonha <paulo@ragonha.me>
2016-02-10 13:29:02 +0100

    Fix Lint errors
    I need to properly configure ESLint to support “exporting and import”

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-export startDevelop from './start-develop'
-export runTest from './run-test'
-export install from './install'
-export buildAndDistribute from './build-and-distribute'
+import _buildAndDistribute from './build-and-distribute'
+import _install from './install'
+import _runTest from './run-test'
+import _startDevelop from './start-develop'
+
+export const buildAndDistribute = _buildAndDistribute
+export const install = _install
+export const runTest = _runTest
+export const startDevelop = _startDevelop
