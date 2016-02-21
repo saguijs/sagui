5155ecdffe94a6e81dcfd97869ac19be45f6d3e9
Paulo Ragonha <paulo@ragonha.me>
2016-02-21 14:56:39 +0100

    Properly handle soft-errors on build (fixes #26)

--- src/actions/build-and-distribute.js
+++ src/actions/build-and-distribute.js
@@ -1,14 +1,30 @@
 import webpack from 'webpack'
 import { logError } from '../util/log'
 
-export default function buildAndDistribute ({ webpackConfig }) {
+export default function buildAndDistribute ({ webpackConfig, watch }) {
   const compiler = webpack(webpackConfig)
 
-  compiler.run(err => {
+  compiler.run((err, stats) => {
+    var softErrors = !err && stats.toJson().errors
+    var hasSoftErrors = softErrors && softErrors.length > 0
+
     if (err) {
-      logError('Build failed.')
-      console.log(err.message || err)
-      process.exit(1)
+      console.error(err.stack || err)
+      if (err.details) console.error(err.details)
+    }
+
+    if (hasSoftErrors) {
+      softErrors.forEach(function (error) {
+        console.error(error)
+      })
+    }
+
+    if (!watch && (err || hasSoftErrors)) {
+      process.on('exit', function () {
+        process.exit(1)
+      })
+
+      return
     }
 
     logError('Build complete, files written in the dist/ folder.')
