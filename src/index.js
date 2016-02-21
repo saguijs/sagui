ef37b2bd03924011f47d0f6a446a3ff827990bed
Paulo Ragonha <paulo@ragonha.me>
2016-02-21 14:14:51 +0100

    Rename “plugins” to “configure”

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import * as actions from './actions'
-import plugins from './plugins'
+import configure from './configure'
 
 export default {
   build (options) {
-    const env = plugins({ ...options, buildTarget: 'build' })
+    const env = configure({ ...options, buildTarget: 'build' })
     actions.buildAndDistribute(env)
   },
 
   dist (options) {
-    const env = plugins({ ...options, buildTarget: 'dist' })
+    const env = configure({ ...options, buildTarget: 'dist' })
     actions.buildAndDistribute(env)
   },
 
   develop (options) {
-    const env = plugins({ ...options, buildTarget: 'develop' })
+    const env = configure({ ...options, buildTarget: 'develop' })
     actions.startDevelop(env)
   },
 
   test (options) {
-    const env = plugins({ ...options, buildTarget: 'test' })
+    const env = configure({ ...options, buildTarget: 'test' })
     actions.runTest(env)
   },
 
   install (options) {
-    const env = plugins(options)
+    const env = configure(options)
     actions.install(env)
   }
 }
