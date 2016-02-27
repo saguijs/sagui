07239be57084fa7af384b473770bf46206950dea
Paulo Ragonha <paulo@ragonha.me>
2016-02-27 21:17:56 +0100

    Refactor pages plugin

--- src/configure/webpack/pages.js
+++ src/configure/webpack/pages.js
@@ -5,24 +5,34 @@ const hotMiddleware = 'webpack-hot-middleware/client'
 
 export default {
   name: 'webpack-pages',
-  configure ({ buildTarget, pages = defaultPages }) {
-    const plugins = pages.map(page => {
-      return new HtmlWebpackPlugin({
-        template: `src/${page}.html`,
-        filename: `${page}.html`,
-        chunks: ['common', page]
-      })
-    })
-
-    let entry = {}
-    pages.forEach(page => {
-      entry[page] = [`./src/${page}`]
-
-      if (buildTarget === 'develop') {
-        entry[page].push(hotMiddleware)
-      }
-    })
+  configure (config) {
+    const entry = configureEntry(config)
+    const plugins = configurePlugins(config)
 
     return { plugins, entry }
   }
+}
+
+function configureEntry ({ pages = defaultPages, buildTarget }) {
+  let entry = {}
+
+  pages.forEach(page => {
+    entry[page] = [`./src/${page}`]
+
+    if (buildTarget === 'develop') {
+      entry[page].push(hotMiddleware)
+    }
+  })
+
+  return entry
+}
+
+function configurePlugins ({ pages = defaultPages }) {
+  return pages.map(page => {
+    return new HtmlWebpackPlugin({
+      template: `src/${page}.html`,
+      filename: `${page}.html`,
+      chunks: ['common', page]
+    })
+  })
 }
