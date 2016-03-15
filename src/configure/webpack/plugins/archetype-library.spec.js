d5caee6344d1dbf762953d7dc066f2e6e3a4479e
Paulo Ragonha <paulo@ragonha.me>
2016-03-15 11:46:25 +0100

    Fix all tests broken after change in Babel “default” exports
    see: https://phabricator.babeljs.io/T2212

--- src/configure/webpack/plugins/archetype-library.spec.js
+++ src/configure/webpack/plugins/archetype-library.spec.js
@@ -1,6 +1,6 @@
 import { join } from 'path'
 import { expect } from 'chai'
-import { configure } from './archetype-library'
+import plugin from './archetype-library'
 
 const saguiPath = join(__dirname, '../../../../')
 const projectPath = join(saguiPath, 'spec/fixtures/library-project')
@@ -14,38 +14,38 @@ describe('configure webpack library', function () {
     }
 
     it('should have a single entry pointing to index.js', function () {
-      const webpackConfig = configure(baseConfiguration)
+      const webpackConfig = plugin.configure(baseConfiguration)
       expect(webpackConfig.entry).eql('./index.js')
     })
 
     it('should have the default exporting target of commonjs2 (module.exports = xxx)', function () {
-      const webpackConfig = configure(baseConfiguration)
+      const webpackConfig = plugin.configure(baseConfiguration)
       expect(webpackConfig.output.libraryTarget).eql('commonjs2')
     })
 
     it('should NOT SET the exporting target if buildTarget is test (a browser wont understand commonjs modules)', function () {
-      const webpackConfig = configure({ ...baseConfiguration, buildTarget: 'test' })
+      const webpackConfig = plugin.configure({ ...baseConfiguration, buildTarget: 'test' })
       expect(webpackConfig.output.libraryTarget).undefined
     })
 
     it('should have the filename as index.js', function () {
-      const webpackConfig = configure(baseConfiguration)
+      const webpackConfig = plugin.configure(baseConfiguration)
       expect(webpackConfig.output.filename).eql('index.js')
     })
 
     it('should have the output path configured as the dist folder', function () {
-      const webpackConfig = configure(baseConfiguration)
+      const webpackConfig = plugin.configure(baseConfiguration)
       expect(webpackConfig.output.path).eql(join(projectPath, 'dist'))
     })
 
     describe('externals', function () {
       it('should infer the externals based on the packgage.json peerDependencies', function () {
-        const webpackConfig = configure(baseConfiguration)
+        const webpackConfig = plugin.configure(baseConfiguration)
         expect(webpackConfig.externals).eql(['react', 'react-dom'])
       })
 
       it('should have an empty externals if the packgage.json does not have a peerDependencies', function () {
-        const webpackConfig = configure({ ...baseConfiguration, projectPath: projectWithoutPeerDependenciesPath })
+        const webpackConfig = plugin.configure({ ...baseConfiguration, projectPath: projectWithoutPeerDependenciesPath })
         expect(webpackConfig.externals).eql([])
       })
     })
--- src/configure/webpack/plugins/json.spec.js
+++ src/configure/webpack/plugins/json.spec.js
@@ -1,10 +1,10 @@
 import { expect } from 'chai'
 
-import { configure } from './json'
+import plugin from './json'
 
 describe('configure webpack json', function () {
   it('should have a JSON loader', function () {
-    const webpackConfig = configure({})
+    const webpackConfig = plugin.configure({})
     const loader = webpackConfig.module.loaders.find(loader => loader.loader === 'json-loader')
     expect(loader.test).eql(/\.(json)$/)
   })
