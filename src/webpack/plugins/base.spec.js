8a6ea948d8a19856d7633650dc5d57d5b17f6397
Paulo Ragonha <paulo@ragonha.me>
2016-05-21 19:58:09 +0200

    Remove no longer needed Uglify test

--- src/webpack/plugins/base.spec.js
+++ src/webpack/plugins/base.spec.js
@@ -9,13 +9,6 @@ const projectPath = join(saguiPath, 'spec/fixtures/simple-project')
 
 describe('configure webpack base', function () {
   describe('targets', function () {
-    it('should have the UglifyJsPlugin enabled while distributing', function () {
-      const config = plugin.configure({ projectPath, saguiPath, buildTarget: 'dist' })
-
-      const commons = config.plugins.filter((plugin) => plugin instanceof optimize.UglifyJsPlugin)
-      expect(commons.length).equal(1)
-    })
-
     it('should have the HotModuleReplacementPlugin enabled while developing', function () {
       const config = plugin.configure({ projectPath, saguiPath, buildTarget: 'development' })
 
