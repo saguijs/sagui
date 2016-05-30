5d07071cdea5ee464526bff1c1bcf8f8147fc13e
Paulo Ragonha <paulo@ragonha.me>
2016-05-30 20:02:21 +0200

    Use standard karma configuration key for webpack

--- template/base/karma.conf.js
+++ template/base/karma.conf.js
@@ -1,10 +1,13 @@
 var sagui = require('sagui')
-var webpackConfig = require('./webpack.config')
+var webpack = require('./webpack.config')
 
 module.exports = sagui().karma({
+  // webpack configuration used to build the tests
+  webpack
+
   /**
    * Additional Karma configuration
    * Will overwrite and extend the default Sagui configuration
    * see: https://karma-runner.github.io/0.13/config/configuration-file.html
    */
-}, webpackConfig)
+})
