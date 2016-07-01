6d51935b5d9539e4fa24604dbedb6b5a073a1c1c
Pierre Wahlgren <pierre.wahlgren@klarna.com>
2016-07-01 19:55:38 +0200

    Divide file extensions into lists and patterns

--- src/file-extensions.js
+++ src/file-extensions.js
@@ -1,13 +1,18 @@
 export default {
-  CSS: /\.css$/,
-  EOT: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-  IMAGE: /\.(png|jpg|jpeg|gif|svg)$/,
-  JAVASCRIPT: /\.(jsx?|es6)$/,
-  JSON: /\.(json)$/,
-  SCSS: /\.scss$/,
-  TTF: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-  VIDEO: /\.(ogg|mp4)$/,
-  WOFF2: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-  WOFF: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-  YAML: /\.(yml|yaml)$/
+  list: {
+    JAVASCRIPT: ['.js', '.jsx', '.es6']
+  },
+  pattern: {
+    CSS: /\.css$/,
+    EOT: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    IMAGE: /\.(png|jpg|jpeg|gif|svg)$/,
+    JAVASCRIPT: /\.(jsx?|es6)$/,
+    JSON: /\.(json)$/,
+    SCSS: /\.scss$/,
+    TTF: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    VIDEO: /\.(ogg|mp4)$/,
+    WOFF2: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+    WOFF: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+    YAML: /\.(yml|yaml)$/
+  }
 }
