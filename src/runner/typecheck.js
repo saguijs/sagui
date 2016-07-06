914b86f5c0351599a9a168c5f0dc65b9bfa942d5
Fernando Via Canel <fernando.canel@klarna.com>
2016-07-06 18:07:09 +0200

    Disable type checking in Windows 😞

--- src/runner/typecheck.js
+++ src/runner/typecheck.js
@@ -1,12 +1,26 @@
 import { execFile } from 'child_process'
 import flow from 'flow-bin'
-import { logError, log } from '../util/log'
+import { logError, logWarning, log } from '../util/log'
 
 const errorCodes = {
   TYPECHECK_ERROR: 2
 }
 
 export default (saguiOptions) => new Promise((resolve, reject) => {
+  // Currently Facebook does not provide Windows builds.
+  // https://github.com/flowtype/flow-bin#flow-bin-
+  //
+  // Instead of using `flow`, we show a warning and ignore this task
+  // For further discussion, you can go to:
+  // https://github.com/saguijs/sagui/issues/179
+  if (process.platform === 'win32') {
+    logWarning('Type checking in Windows is not currently supported')
+    log('Official flow builds for Windows are not currently provided.')
+    log('We are exploring options in https://github.com/saguijs/sagui/issues/179')
+
+    return resolve()
+  }
+
   const commandArgs = ['check', '--color=always']
 
   if (saguiOptions.javaScript && saguiOptions.javaScript.typeCheckAll) {
