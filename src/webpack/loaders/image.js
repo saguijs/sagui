f11e457248cfd6c5f775be196ee13fb185236476
Paulo Ragonha <paulo@ragonha.me>
2016-07-22 09:05:16 +0200

    Rollback setup of null-loader for images while running tests 👎
    The impact in performance is negligible.

--- src/webpack/loaders/image.js
+++ src/webpack/loaders/image.js
@@ -1,21 +1,14 @@
 import fileExtensions from '../../file-extensions'
-import actions from '../../actions'
 
 export default {
   name: 'image',
   configure ({ action }) {
-    // User null-loader during tests
-    // for better performance
-    const loader = action === actions.TEST
-      ? 'null-loader'
-      : 'url-loader?limit=8192&name=[name]-[hash].[ext]'
-
     return {
       module: {
         loaders: [
           {
             test: fileExtensions.test.IMAGE,
-            loader
+            loader: 'url-loader?limit=8192&name=[name]-[hash].[ext]'
           }
         ]
       }
