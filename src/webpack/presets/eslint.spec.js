f468bc41dd805fc87cb1d6cc8dd6c3e17ba4cb20
Paulo Ragonha <paulo@ragonha.me>
2016-09-12 17:21:10 +0200

    Remove “debugger” rule exception on development (it no longer break the build) 💀

--- src/webpack/presets/eslint.spec.js
+++ src/webpack/presets/eslint.spec.js
@@ -1,32 +1,14 @@
 import { expect } from 'chai'
+import path from 'path'
 import preset from './eslint'
 import actions from '../../actions'
 
 describe('eslint webpack preset', function () {
-  describe('when watch is enabled', () => {
-    it('should set "no-debugger" rule to 0', function () {
-      const projectPath = 'a/demo/path'
-      const config = preset.configure({ watch: true, projectPath })
+  it('should point to a project eslintrc file', () => {
+    const projectPath = 'a/demo/path'
+    const action = actions.BUILD
+    const config = preset.configure({ action, projectPath })
 
-      expect(config.eslint.rules['no-debugger']).to.eql(0)
-    })
-  })
-  describe('when action is "develop"', () => {
-    it('should set "no-debugger" rule to 0', function () {
-      const projectPath = 'a/demo/path'
-      const action = actions.DEVELOP
-      const config = preset.configure({ action, projectPath })
-
-      expect(config.eslint.rules['no-debugger']).to.eql(0)
-    })
-  })
-  describe('when action is not "develop"', () => {
-    it('should not set any rules', () => {
-      const projectPath = 'a/demo/path'
-      const action = actions.BUILD
-      const config = preset.configure({ action, projectPath })
-
-      expect(config.eslint.rules).to.eql({})
-    })
+    expect(config.eslint.configFile).to.eql(path.join(projectPath, '.eslintrc'))
   })
 })
