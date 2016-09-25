d4133a7984621a54543ab3ecf9a0b5b5b47eff2c
Paulo Ragonha <paulo@ragonha.me>
2016-09-25 11:33:05 +0200

    Run tests with any extension if it matches `.spec.` 📈

--- karma-static-files/test-bundle.js
+++ karma-static-files/test-bundle.js
@@ -1,2 +1,2 @@
-const testsContext = require.context('../../../src', true, /\.spec\.js$/)
+const testsContext = require.context('../../../src', true, /\.spec\..+$/)
 testsContext.keys().forEach(testsContext)
