d686b3b2e0c0f6b3f49613bc3fc5fc75566534c8
Xavier Via <fernando.via@gmail.com>
2016-09-26 18:05:43 +0200

    Inspect AppVeyor's env

--- src/runner/typecheck.spec.js
+++
@@ -1,9 +0,0 @@
-import flow from 'flow-bin'
-import { notEqual } from 'assert'
-
-describe('typecheck', () => {
-  it('should have a path for flow-bin', () => {
-    console.log(flow)
-    notEqual(flow, null)
-  })
-})
