97e4a21db66083e1f6058a3445c0f743d201c01e
Paulo Ragonha <paulo@ragonha.me>
2016-12-30 14:47:22 +0100

    Move NODE_ENV definition to the CLI 👟
    It is a CLI concern, and that way it is available everywhere.

--- src/cli/index.js
+++ src/cli/index.js
@@ -24,7 +24,9 @@ export default (argv = []) => {
   if (!argv.slice(2).length) program.outputHelp()
 }
 
-const setupAction = (action) => (cliOptions = {}) => {
+const setupAction = (NODE_ENV, action) => (cliOptions = {}) => {
+  process.env.NODE_ENV = NODE_ENV
+
   const options = {
     ...cliOptions,
     action,
@@ -39,27 +41,27 @@ const setupAction = (action) => (cliOptions = {}) => {
 program.command(actions.DEVELOP)
   .description('Run development environment')
   .option('-p, --port <n>', 'Port the server will listen (default: 3000)', parseInt)
-  .action(setupAction(actions.DEVELOP))
+  .action(setupAction('development', actions.DEVELOP))
 
 program.command(actions.BUILD)
   .description('Build the project (production environment)')
   .option('-p, --optimize', 'Optimize the build (minify, dedup...)')
-  .action(setupAction(actions.BUILD))
+  .action(setupAction('production', actions.BUILD))
 
 program.command(actions.TEST_UNIT)
   .description('Run unit tests')
   .option('-w, --watch', 'Run tests on any file change')
   .option('-c, --coverage', 'Generate a coverage report')
-  .action(setupAction(actions.TEST_UNIT))
+  .action(setupAction('test', actions.TEST_UNIT))
 
 program.command(actions.TEST_LINT)
   .description('Lint the code')
-  .action(setupAction(actions.TEST_LINT))
+  .action(setupAction('test', actions.TEST_LINT))
 
 program.command(actions.TEST_TYPECHECK)
   .description('Typecheck the code')
-  .action(setupAction(actions.TEST_TYPECHECK))
+  .action(setupAction('test', actions.TEST_TYPECHECK))
 
 program.command(actions.UPDATE)
   .description('Update Sagui in the current project')
-  .action(setupAction(actions.UPDATE))
+  .action(setupAction('development', actions.UPDATE))
