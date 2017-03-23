be278e5e3ada42e05b1f9c0069b8b27c87168c1f
Nikola Pejoski <nikola.pejoski@klarna.com>
2017-03-23 16:05:45 +0100

    Run clean only when the action is BUILD 🏗

--- src/webpack/presets/clean.js
+++ src/webpack/presets/clean.js
@@ -1,15 +1,16 @@
 import CleanWebpackPlugin from 'clean-webpack-plugin'
+import actions from '../../actions'
 
 export default {
   name: 'clean',
-  configure ({ projectPath }) {
+  configure ({ projectPath, action }) {
     return {
-      plugins: [
+      plugins: action === actions.BUILD ? [
         new CleanWebpackPlugin(['dist'], {
           root: projectPath,
           verbose: false
         })
-      ]
+      ] : []
     }
   }
 }
