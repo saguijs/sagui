2e89e4e6079415671338fbb3b4beb694457ea9b6
Paulo Ragonha <paulo@ragonha.me>
2017-05-09 23:44:43 +0200

    Log that we are formatting files 👄

--- src/run/format.js
+++ src/run/format.js
@@ -1,19 +1,35 @@
 import prettier from 'prettier'
+import chalk from 'chalk'
 import glob from 'glob'
 import path from 'path'
 import fs from 'fs'
 import eslintConfig from '../configure-webpack/loaders/javascript-eslintrc.json'
+import { log } from '../util/log'
 
 const prettierOptions = eslintConfig.rules['prettify/prettier'][1]
 
 export default (saguiConfig) => new Promise((resolve, reject) => {
+  log('Formatting files...')
   glob(path.join(saguiConfig.projectPath, 'src/**/*.js'), (error, files) => {
     if (error) { return reject(error) }
 
     try {
-      files.forEach((file) => {
-        fs.writeFileSync(file, prettier.format(fs.readFileSync(file).toString(), prettierOptions))
+      const formatted = files.map((file) => {
+        const original = fs.readFileSync(file).toString()
+        const formatted = prettier.format(original, prettierOptions)
+
+        return [file, original, formatted]
+      }).filter(([file, original, formatted]) => {
+        const changed = original.indexOf(formatted)
+        const color = changed ? chalk.green : chalk.dim
+
+        console.log(color(file.replace(`${saguiConfig.projectPath}`, '')))
+        return changed
       })
+
+      formatted.forEach(([file, original, formatted]) => fs.writeFileSync(file, formatted))
+
+      log(`Formatted ${formatted.length} files.`)
     } catch (error) {
       return reject(error)
     }
