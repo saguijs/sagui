b01b6c4652c98dc1bc584b70dfabdb3eb5d7265e
Xavier Via <fernando.via@gmail.com>
2017-05-29 18:00:34 +0200

    Add test skeleton for updateGitignore

---
+++ src/run/update/update-gitignore.spec.js
@@ -0,0 +1,17 @@
+// import { expect } from 'chai'
+// import updateGitignore from './update-gitignore'
+
+describe('updateGitignore', () => {
+  describe('given the file does not exist', () => {
+    it('should create the .gitignore file')
+  })
+
+  describe('given the file exists and it doesn’t have any of Sagui’s entries', () => {
+    // Make sure example was not alphabetical to start with
+    it('should append Sagui entries without changing the order of the existing ones')
+  })
+
+  describe('given exists and has some Sagui entries', () => {
+    it('should append the missing ones with no duplicates')
+  })
+})
