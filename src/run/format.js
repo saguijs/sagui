fb15e0e9a1f3682e482ae16ab313c3ff5e43f9ee
Paulo Ragonha <paulo@ragonha.me>
2017-06-16 17:03:36 +0200

    It should also format the `sagui.config.js` 👷‍♀️

--- src/run/format.js
+++ src/run/format.js
@@ -11,35 +11,35 @@ const prettierOptions = eslintConfig.rules['prettify/prettier'][1]
 export default saguiConfig =>
   new Promise((resolve, reject) => {
     log('Formatting files...')
-    glob(path.join(saguiConfig.projectPath, 'src/**/*.{js,jsx,es6}'), (error, files) => {
-      if (error) {
-        return reject(error)
-      }
 
-      try {
-        const formatted = files
-          .map(file => {
-            const original = fs.readFileSync(file).toString()
-            const formatted = prettier.format(original, prettierOptions)
+    try {
+      const files = [
+        ...glob.sync(path.join(saguiConfig.projectPath, 'sagui.config.js')),
+        ...glob.sync(path.join(saguiConfig.projectPath, 'src/**/*.{js,jsx,es6}'))
+      ]
 
-            return [file, original, formatted]
-          })
-          .filter(([file, original, formatted]) => {
-            const changed = original.indexOf(formatted)
-            const color = changed ? chalk.green : chalk.dim
-            const relativePath = path.relative(saguiConfig.projectPath, file)
+      const formatted = files
+        .map(file => {
+          const original = fs.readFileSync(file).toString()
+          const formatted = prettier.format(original, prettierOptions)
 
-            console.log(color(relativePath))
-            return changed
-          })
+          return [file, original, formatted]
+        })
+        .filter(([file, original, formatted]) => {
+          const changed = original.indexOf(formatted)
+          const color = changed ? chalk.green : chalk.dim
+          const relativePath = path.relative(saguiConfig.projectPath, file)
 
-        formatted.forEach(([file, original, formatted]) => fs.writeFileSync(file, formatted))
+          console.log(color(relativePath))
+          return changed
+        })
 
-        log(`Formatted ${formatted.length} files.`)
-      } catch (error) {
-        return reject(error)
-      }
+      formatted.forEach(([file, original, formatted]) => fs.writeFileSync(file, formatted))
+
+      log(`Formatted ${formatted.length} files.`)
 
       resolve()
-    })
+    } catch (error) {
+      return reject(error)
+    }
   })
